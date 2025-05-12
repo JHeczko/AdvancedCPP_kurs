9d8f7398343b2dbfbcf1baa4af25a18b95fbfc0e
JHeczko <jakub.heczko5@gmail.com>
2025-05-12 18:54:57 +0200

    zestaw 9 another half done

--- zestaw9/zadanie5.cpp
+++ zestaw9/zadanie5.cpp
@@ -3,3 +3,30 @@
 * wywołanie destruktora w przypadku znikania ostatniego z nich
 * */
 
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class A {
+  public:
+
+  ~A() {
+    cout << "'A' znika" << endl;
+  }
+};
+
+int main() {
+  shared_ptr<A> p = make_shared<A>();
+  shared_ptr<A> q = p;
+
+  cout << "Poczatek: " << p.use_count() << endl;
+
+  q = nullptr;
+
+  cout << "Jeszcze nie znika: " << p.use_count() << endl;
+
+  p = nullptr;
+
+  cout << "Powinno juz zniknac, do tego momentu :D";
+}
--- zestaw9/zadanie6.md
+++ zestaw9/zadanie6.md
@@ -1,2 +1,4 @@
 # Jakie jest działanie std::weak_ptr ? Przedstaw przykład użycia.
 
+## `weak_ptr`
+Rozni sie on tym, ze nie jest on "wlasniosciowym" wksaznikiem. W praktyce oznacza to tyle, ze nie zwieksza ilosc referencji do obiektu 
